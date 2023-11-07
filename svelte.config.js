84c999e923a47e4947ad1b74a0c979ce53707252
Carson Haskell <carson.haskell@gmail.com>
2023-11-07 15:21:22 -0800

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	},
 	preprocess: vitePreprocess()
 };
